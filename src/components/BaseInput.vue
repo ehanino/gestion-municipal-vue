d14fa0437152d6a14a5210add88b0fb61024bfe8
ehanino <eha.nino@gmail.com>
2024-08-14 17:20:11 -0500

    14 Ago Ofi

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="content">
+        <input @blur="handleBlur" v-model="username" name="username" type="text" class="input username" placeholder="Username" />
+        <div class="user-icon"></div>
+        <select class="input mt-1" name="select-apps" id="select-apps"></select>
+        <input v-model="password" name="password" type="password" class="input mt-1" placeholder="Password" />
+        <div class="pass-icon"></div>
+    </div>
+</template>
